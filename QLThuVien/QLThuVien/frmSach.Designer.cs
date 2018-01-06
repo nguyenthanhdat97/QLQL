f1fb1189e2a35931cdc54df0e5d52cd4883e2fe5
gachj <gachj_bmt@yahoo.com>
2018-01-07 00:28:25 +0700

    Update form sach designer

--- QLThuVien/QLThuVien/frmSach.Designer.cs
+++ QLThuVien/QLThuVien/frmSach.Designer.cs
@@ -19,5 +19,49 @@
             }
             base.Dispose(disposing);
         }
+
+	private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label btntensach;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txttheloaisach;
+        private System.Windows.Forms.TextBox txtTensach;
+        private System.Windows.Forms.TextBox txttg;
+        private System.Windows.Forms.TextBox txtMasach;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtsoluong;
+        private System.Windows.Forms.TextBox txtnamxb;
+        private System.Windows.Forms.TextBox txttinhtrang;
+        private System.Windows.Forms.TextBox txtnxb;
+        private System.Windows.Forms.PictureBox PICHINH;
+        private System.Windows.Forms.Button BTNHINH;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvttsach;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lbltong;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnxoasach;
+        private System.Windows.Forms.Button btnthemsach;
+        private System.Windows.Forms.Button btnsuasach;
+        private System.Windows.Forms.Button btnluusach;
+        private System.Windows.Forms.Label lblhienthinv;
+        private System.Windows.Forms.Label lblthongbao;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lbltieude;
+        private System.Windows.Forms.TextBox txtpage;
+        private System.Windows.Forms.Button btndau;
+        private System.Windows.Forms.Button btnlui;
+        private System.Windows.Forms.Button btntoi;
+        private System.Windows.Forms.Button btncuoi;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btntimkiemttv;
+        private System.Windows.Forms.TextBox txttimkiemma;
+        private System.Windows.Forms.Button btnin1;
     }
 }
