2608769f1b1129e244b2fe2012bc996b3a820bee
gachj <gachj_bmt@yahoo.com>
2018-01-06 14:18:41 +0700

    images

---
+++ QLThuVien/QLThuVien/frmNhanVien.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QLThuVien
+{
+    public partial class frmNhanVien : Form
+    {
+	string A;
+        SqlConnection cnn;
+        public frmNhanVien()
+        {
+            InitializeComponent();
+            cnn = new SqlConnection("Data Source=.;Initial Catalog=QLThuVien;Integrated Security=True");
+        }
+	
+        
+    }
+}
