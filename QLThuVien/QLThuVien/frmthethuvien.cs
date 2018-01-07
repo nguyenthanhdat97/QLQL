73ae05fcf1f1b9b4a032092ab10d040f290658f2
gachj <gachj_bmt@yahoo.com>
2018-01-08 00:18:24 +0700

    Form the thu vien

---
+++ QLThuVien/QLThuVien/frmthethuvien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QLThuVien
+{
+    public partial class frmthethuvien : Form
+    {
+        SqlConnection cnn;
+        DataTable vtttv;
+        int vt;
+        public frmthethuvien()
+        {
+            InitializeComponent();
+            cnn = new SqlConnection("Data Source=.;Initial Catalog=QLThuVien;Integrated Security=True");
+        } 
+    }
+}
