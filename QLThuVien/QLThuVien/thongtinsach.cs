c864f48417a28fb73c3f950042dffe7e9a324420
gachj <gachj_bmt@yahoo.com>
2018-01-07 17:15:06 +0700

    Update thong tin sach cs

--- QLThuVien/QLThuVien/thongtinsach.cs
+++ QLThuVien/QLThuVien/thongtinsach.cs
@@ -20,6 +20,61 @@ namespace QLThuVien
             InitializeComponent();
             cnn = new SqlConnection("Data Source=.;Initial Catalog=QLThuVien;Integrated Security=True");
         }
-        
+        private DataTable docsachhu()
+        {
+            string chuoikn = "select * from SACH where TinhTrang=N'Cũ' or TinhTrang=N'Hơi cũ' or TinhTrang=N'Chữ Hơi mờ' "; 
+            SqlCommand cmd = new SqlCommand(chuoikn, cnn);
+            DataTable sachhu = new DataTable();
+            cnn.Open();
+            sachhu.Load(cmd.ExecuteReader());
+            cnn.Close();
+            return sachhu;
+        }        
+        private DataTable docsachbanchay()
+        {
+            string chuoikn = "select * from SACH WHERE MaSach  IN(select MaSach from PHIEUMUON WHERE SACH.MaSach=PHIEUMUON.MaSach)";
+            SqlCommand cmd = new SqlCommand(chuoikn, cnn);
+            DataTable sach = new DataTable();
+            cnn.Open();
+            sach.Load(cmd.ExecuteReader());
+            cnn.Close();
+            return sach;
+        }
+        private DataTable docsachtonkho()
+        {
+            string chuoikn = "select * from SACH where MaSach not in(select MaSach from SACHMUON )";
+            SqlCommand cmd = new SqlCommand(chuoikn, cnn);
+            DataTable sach = new DataTable();
+            cnn.Open();
+            sach.Load(cmd.ExecuteReader());
+            cnn.Close();
+            return sach;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (chsachhu.Checked)
+            {
+                dgvthongtin.DataSource=docsachhu();
+                txttong.ForeColor = Color.Red;
+                txttong.Text = (dgvthongtin.Rows.Count).ToString();
+            }
+            if (chsachkbd.Checked)
+            {
+                dgvthongtin.DataSource = docsachtonkho();
+                txttong.ForeColor = Color.Red;
+                txttong.Text = (dgvthongtin.Rows.Count).ToString();
+            }
+            if (chsachbandc.Checked)
+            {
+                dgvthongtin.DataSource = docsachbanchay();
+                txttong.ForeColor = Color.Red;
+                txttong.Text = (dgvthongtin.Rows.Count).ToString();
+            }
+        }
+
+        private void thongtinsach_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
