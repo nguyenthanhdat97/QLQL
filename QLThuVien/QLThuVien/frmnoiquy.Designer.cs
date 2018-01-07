0d32d9b14f2a8176af9f2f1255bf9360519d5bec
gachj <gachj_bmt@yahoo.com>
2018-01-07 11:13:27 +0700

    Update form noi quy designer

--- QLThuVien/QLThuVien/frmnoiquy.Designer.cs
+++ QLThuVien/QLThuVien/frmnoiquy.Designer.cs
@@ -19,7 +19,8 @@
             }
             base.Dispose(disposing);
         }
-	#region Windows Form Designer generated code
+
+        #region Windows Form Designer generated code
 
         /// <summary>
         /// Required method for Designer support - do not modify
